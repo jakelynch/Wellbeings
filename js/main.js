15c98aa96e6efff680124d7df1b5964e8cd90939
Jimmy Martter <techgods@Techs-MacBook-Pro.local>
2016-04-07 14:51:10 -0400

    jm

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+	if (localStorage.getItem("session") != null) {
+	    showpage(#home);
+	}
+	else {
+		showpage(#login);
+	}
+}
+
 function showpage(id){
     
     $(".page").hide();
